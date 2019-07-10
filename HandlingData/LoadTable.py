14b5d5a9666c495cea1bc4c78b7121c58203dec6
Satoru Hayasaka <hayasaka@utexas.edu>
2019-07-10 12:36:59 -0500

    Loading the table from a file

--- HandlingData/LoadTable.py
+++ HandlingData/LoadTable.py
@@ -1,2 +1,9 @@
 import os
-import pandas
+import pandas as pd
+
+# data directory & file
+dataDir = '/tmp/Data/ds102'
+fileTable = os.path.join(dataDir,'participants.tsv')
+
+# Loading the data file as a data frame object
+ptData = pd.read_csv(fileTable, sep='\t')
