6fe9b83dd3f2d3395f239b1b8b708c9752dca410
Satoru Hayasaka <hayasaka@utexas.edu>
2019-07-15 13:02:37 -0500

    Directory adjusted for Jupyter

--- HandlingData/ReadImageData.py
+++ HandlingData/ReadImageData.py
@@ -2,11 +2,10 @@ import os
 import numpy as np
 import nibabel as nib
 
-# Directory where your data set resides. This needs to be customized
-dataDir = '/home/satoru/Teaching/fMRI_Fall_2018/Data/ds102'
+# Directory where your data set resides.
+dataDir = '/tmp/Data/ds102'
 
-# paths for the structural and functional MRI data. 
-# NOTE that the paths are different for each user. You need to customize
+# paths for the structural and functional MRI data.
 f_sMRI = os.path.join(dataDir,'sub-26/anat/sub-26_T1w.nii.gz')
 f_fMRI = os.path.join(dataDir,'sub-26/func/sub-26_task-flanker_run-2_bold.nii.gz')
 
@@ -18,5 +17,3 @@ X_sMRI = sMRI.get_data()
 # fMRI
 fMRI = nib.load(f_fMRI)
 X_fMRI = fMRI.get_data()
-
-
