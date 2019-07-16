4456d343b5f92730c858f456187515ded9d9a8b6
Satoru Hayasaka <hayasaka@utexas.edu>
2019-07-16 12:32:45 -0500

    added average image in x-direction

--- HandlingData/ShowT1Image.py
+++ HandlingData/ShowT1Image.py
@@ -2,4 +2,27 @@ import os
 import numpy as np
 import nibabel as nib
 import matplotlib.pyplot as plt
-from nilearn import plotting
+from nilearn.plotting import plot_anat
+
+
+# Directory where your data set resides.
+dataDir = '/tmp/Data/ds102'
+
+# reading in the T1 image data array
+f_sMRI = os.path.join(dataDir,'sub-26/anat/sub-26_T1w.nii.gz')
+sMRI = nib.load(f_sMRI)
+X_sMRI = sMRI.get_data()
+
+# displaying image
+plot_anat(f_sMRI,
+          display_mode='ortho',
+          dim=-1,
+          draw_cross=True,
+          annotate=True)
+
+# average intensity in the x-direction
+avgX_X_sMRI = X_sMRI.mean(axis=0)
+
+# showing the resulting image
+plt.imshow(np.rot90(avgX_X_sMRI), cmap='gray')
+plt.axis('off')
