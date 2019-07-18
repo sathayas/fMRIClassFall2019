904f16cb1b4340d6b9f1eb6fe8c4e26a28c566ba
Satoru Hayasaka <hayasaka@utexas.edu>
2019-07-18 12:00:38 -0500

    Initial commit; copied from last year

---
+++ NIfTI_BIDS/ViewHeader.py
@@ -0,0 +1,26 @@
+import os
+import numpy as np
+import nibabel as nib
+
+
+# Directory where your data set resides. This needs to be customized
+dataDir = '/home/satoru/Teaching/fMRI_Fall_2018/Data/ds102'
+
+# reading in the fMRI data array
+f_fMRI = os.path.join(dataDir,'sub-26/func/sub-26_task-flanker_run-2_bold.nii.gz')
+fMRI = nib.load(f_fMRI)   # image object
+
+# priting out the header information
+hdr_fMRI = fMRI.header
+print(hdr_fMRI)
+
+
+# image dimension
+print(hdr_fMRI.get_data_shape())
+
+# data type
+print(hdr_fMRI.get_data_dtype())
+
+# voxel size
+print(hdr_fMRI.get_zooms())
+
