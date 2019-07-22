759de03c07198f0237e87a9c0972f7a8d1bbac5d
Satoru Hayasaka <hayasaka@utexas.edu>
2019-07-22 12:15:51 -0500

    Loading data

--- MoCor/PlotMoPar.py
+++ MoCor/PlotMoPar.py
@@ -1,3 +1,11 @@
-import numpy as np
 import os
+import numpy as np
+import pandas as pd
 import matoplotlib.pyplot as plt
+
+# loading the data
+resDir = '/tmp/Data/ds102/WorkflowOutput/Results/par_file'
+resFname = 'sub-26_task-flanker_run-1_bold_roi_mcf.nii.gz.par'
+moData = pd.read_csv(os.path.join(resDir,resFname),
+                     header=None,
+                     delimiter=r"\s+")
