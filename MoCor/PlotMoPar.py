7b51fb05998ebce6a048b0cb0e057d6168bd7591
Satoru Hayasaka <hayasaka@utexas.edu>
2019-07-22 12:50:02 -0500

    Rotation and shift parameter plots added

--- MoCor/PlotMoPar.py
+++ MoCor/PlotMoPar.py
@@ -1,7 +1,7 @@
 import os
 import numpy as np
 import pandas as pd
-import matoplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 
 # loading the data
 resDir = '/tmp/Data/ds102/WorkflowOutput/Results/par_file'
@@ -9,3 +9,24 @@ resFname = 'sub-26_task-flanker_run-1_bold_roi_mcf.nii.gz.par'
 moData = pd.read_csv(os.path.join(resDir,resFname),
                      header=None,
                      delimiter=r"\s+")
+
+# plotting rotations
+plt.plot(moData[0],'r', label='roll')
+plt.plot(moData[1],'g', label='picth')
+plt.plot(moData[2],'b', label='yaw')
+plt.title('Rotation')
+plt.xlabel('time points')
+plt.ylabel('ratation (radian)')
+plt.legend()
+plt.show()
+
+
+# plotting shifts
+plt.plot(moData[3],'r', label='x')
+plt.plot(moData[4],'g', label='y')
+plt.plot(moData[5],'b', label='z')
+plt.title('Shift')
+plt.xlabel('time points')
+plt.ylabel('shift (mm)')
+plt.legend()
+plt.show()
