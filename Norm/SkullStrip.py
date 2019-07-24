5c50f87e3c1b020b002614e8dfddb838abb092b6
Satoru Hayasaka <hayasaka@utexas.edu>
2019-07-24 13:26:51 -0500

    A bunch of libraries are imported; now it uses BIDS layout to specify files

--- Norm/SkullStrip.py
+++ Norm/SkullStrip.py
@@ -4,10 +4,19 @@ import nibabel as nib
 import matplotlib.pyplot as plt
 import nipype.interfaces.fsl as fsl # importing FSL interface functions
 from nilearn.plotting import plot_anat, plot_epi, view_img
+from nipype import Node, Workflow  # components to construct workflow
+from nipype.interfaces.io import DataSink  # datasink
+from bids.layout import BIDSLayout  # BIDSLayout object to specify file(s)
 
 
 # Directory where your data set resides.
 dataDir = '/tmp/Data/ds102'
 
-# an T1 weighted image from one of the subjects
-imageT1 = os.path.join(dataDir,'sub-26/anat/sub-26_T1w.nii.gz')
+# Creating the layout object for this BIDS data set
+layout = BIDSLayout(dataDir)
+
+# T1 image from the layout object
+imageT1 = layout.get(subject='26',
+                       suffix='T1w',
+                       extension='nii.gz',
+                       return_type='file')[0]
