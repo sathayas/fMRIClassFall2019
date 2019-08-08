f5eaf6f39274ce4bf161a2702285d8d8c8431f37
Satoru Hayasaka <hayasaka@utexas.edu>
2019-08-08 13:31:04 -0500

    duplicate lines were deleted

--- Norm/Norm.py
+++ Norm/Norm.py
@@ -28,31 +28,10 @@ fMNI = '/usr/share/fsl/data/standard/MNI152_T1_2mm_brain.nii.gz'
 
 # Output directory
 outDir = os.path.join(dataDir, 'WorkflowOutput')
-import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
-import nipype.interfaces.fsl as fsl # importing FSL interface functions
-from nipype import Node, Workflow  # components to construct workflow
-from nipype.interfaces.io import DataSink  # datasink
-from bids.layout import BIDSLayout  # BIDSLayout object to specify file(s)
-from nilearn import image
-from nilearn.plotting import plot_anat, view_img
-
-
-# Directory where your data set resides.
-dataDir = '/tmp/Data/ds102'
-
-# Creating the layout object for this BIDS data set
-layout = BIDSLayout(dataDir)
-
-# an T1 weighted image from one of the subjects
-imageT1 = layout.get(subject='26',
-                     suffix='T1w',
-                     extension='nii.gz',
-                     return_type='file')[0]
 
 # template image (from FSL)
 fMNI = '/usr/share/fsl/data/standard/MNI152_T1_2mm_brain.nii.gz'
-fMNISingle =
+
 # Output directory
 outDir = os.path.join(dataDir, 'WorkflowOutput')
 
@@ -128,7 +107,7 @@ display = plot_anat(imageNormLinT1,
 display.add_edges(fMNI)
 
 # displaying the normalized T1 (coronal)
-display = plot_anat(imageNormLinT1,
+display = plot_anat(imageNormLinT1,adjustment
                     display_mode='y',
                     cut_coords=[-60,-30,0,30])
 
