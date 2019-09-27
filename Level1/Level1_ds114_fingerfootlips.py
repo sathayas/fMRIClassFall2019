6b9d84c85f7b9b598d48cbda10c690f968ed8f01
Satoru Hayasaka <hayasaka@utexas.edu>
2019-09-27 10:39:38 -0500

    everything is updated to run on fMRIprep data

--- Level1/Level1_ds114_fingerfootlips.py
+++ Level1/Level1_ds114_fingerfootlips.py
@@ -10,9 +10,15 @@ from nipype.algorithms import modelgen  # GLM model generator
 from nipype.interfaces.base import Bunch
 from bids.layout import BIDSLayout  # BIDSLayout object to specify file(s)
 
-
-# data directory
+##### DIRECTORY BUSINESS ######
+# original data directory
 dataDir = '/tmp/Data/ds114'
+# Output directory
+outDir = os.path.join(dataDir,'WorkflowOutput')
+
+
+##### PARAMETERS TO BE USED #####
+nDelfMRI = 4  # number of first scans to be deleted
 
 ###########
 #
@@ -47,7 +53,7 @@ fileMask = os.path.join(sesDir,imageMask)
 
 # skip dummy scans
 extract = Node(fsl.ExtractROI(in_file=filefMRI,  # input image full path
-                              t_min=4,            # first 4 volumes are deleted
+                              t_min=nDelfMRI,    # first volumes to be deleted
                               t_size=-1),
                name="extract")
 
@@ -61,37 +67,35 @@ applymask = Node(fsl.ApplyMask(mask_file=fileMask),
                  name='applymask')
 
 
-
-##### Start from Here
+###########
+#
+# READ TASK INFO IN PREPRARATION FOR THE ANALYSIS
+#
+###########
 
 # Creating the layout object for this BIDS data set
 layout = BIDSLayout(dataDir)
 
-
 # task information file
-eventFile = layout.get(type='events',
+fileEvent = layout.get(suffix='events',
                        task='fingerfootlips',
-                       extensions='tsv',
+                       extension='tsv',
                        return_type='file')[0]
 
-
-# Output directory
-outDir = os.path.join(dataDir,'WorkflowOutput')
-
 # getting TR from the image header
-fMRI = nib.load(imagefMRI)   # image object
+fMRI = nib.load(filefMRI)   # image object
 hdr_fMRI = fMRI.header
 TR = hdr_fMRI['pixdim'][4]
-
+onsetOffset = nDelfMRI * TR  # time adjustement due to deleted fMRI volumes
 
 ## Getting experiment info from the event file, into a Bunch object
-trialInfo = pd.read_table(eventFile)
+trialInfo = pd.read_csv(fileEvent, sep='\t')
 conditions = sorted(list(set(trialInfo.trial_type)))
 onsets = []
 durations = []
 
 for itrial in conditions:
-    onsets.append(list(trialInfo[trialInfo.trial_type==itrial].onset-10)) # subtracting 10s due to removing of 4 dummy scans
+    onsets.append(list(trialInfo[trialInfo.trial_type==itrial].onset-onsetOffset))
     durations.append(list(trialInfo[trialInfo.trial_type==itrial].duration))
 
 subject_info = [Bunch(conditions=conditions,
@@ -114,9 +118,15 @@ cont08 = ['activation',     'F', [cont02, cont03, cont04]]
 contrast_list = [cont01, cont02, cont03, cont04, cont05, cont06, cont07, cont08]
 
 
+
+###########
+#
+# SETTING UP THE FIRST LEVEL ANALYSIS NODES
+#
+###########
+
 # model specification
-modelspec = Node(modelgen.SpecifyModel(functional_runs=imagefMRI,
-                                       subject_info=subject_info,
+modelspec = Node(modelgen.SpecifyModel(subject_info=subject_info,
                                        input_units='secs',
                                        time_repetition=TR,
                                        high_pass_filter_cutoff=100),
@@ -142,16 +152,35 @@ datasink = Node(DataSink(base_directory=outDir),
                 name='datasink')
 
 
+###########
+#
+# SETTING UP THE WORKFLOW NODES
+#
+###########
+
 # creating the workflow
-firstLevel = Workflow(name="Level1_FSL", base_dir=outDir)
+firstLevel = Workflow(name="Level1_FingerFootLips", base_dir=outDir)
 
 # connecting nodes
-firstLevel.connect([(modelspec, level1design, [('session_info', 'session_info')])])
-firstLevel.connect([(level1design, modelgen, [('fsf_files', 'fsf_file'),
-                                              ('ev_files','ev_files')])])
-firstLevel.connect([(level1design, feat, [('fsf_files', 'fsf_file')])])
-firstLevel.connect([(feat, datasink, [('feat_dir', 'FSL_Level1.@feat')])])
+firstLevel.connect(extract, 'roi_file', susan, 'in_file')
+firstLevel.connect(susan, 'smoothed_file', applymask, 'in_file')
+firstLevel.connect(applymask, 'out_file', modelspec, 'functional_runs')
+firstLevel.connect(modelspec, 'session_info', level1design, 'session_info')
+firstLevel.connect(level1design, 'fsf_files', modelgen, 'fsf_file')
+firstLevel.connect(level1design, 'ev_files', modelgen, 'ev_files')
+firstLevel.connect(level1design, 'fsf_files', feat, 'fsf_file')
+firstLevel.connect(feat, 'feat_dir', datasink, 'feat_dir')
+firstLevel.connect(applymask, 'out_file', datasink, 'preproc_out_file')
 
+# writing out graphs
+firstLevel.write_graph(graph2use='orig', dotfilename='graph_orig.dot')
+
+# showing the graph
+plt.figure(figsize=[6,6])
+img=mpimg.imread(os.path.join(outDir,"Level1_FingerFootLips","graph_orig.png"))
+imgplot = plt.imshow(img)
+plt.axis('off')
+plt.show()
 
 # running the workflow
 firstLevel.run()
