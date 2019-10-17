731b76d121dd9660f5b4039d6c38db1e181520d0
Satoru Hayasaka <hayasaka@utexas.edu>
2019-10-17 10:02:43 -0500

    2nd level analysis, finger-foot-lips task, test session, cope5 (finger vs others); initial commit

---
+++ Level2/Level2_FingerFootLips_Test_Cope5.py
@@ -0,0 +1,88 @@
+import os  # system functions
+import nipype.interfaces.fsl as fsl  # fsl
+from nipype import Node, Workflow  # components to construct workflow
+from nipype import SelectFiles  # to facilitate file i/o
+from nipype.interfaces.io import DataSink  # datasink
+
+
+##### PARAMETERS #####
+indCope = '5'  # the contrast of interest, finger vs others
+indSes = 'test'  # the session of interest
+
+
+##### DIRECTORY BUSINESS ######
+# original data directory
+dataDir = '/tmp/Data/ds114'
+# Output directory
+outDir = os.path.join(dataDir,'WorkflowOutput')
+
+
+
+###########
+#
+# A LIST OF COPE AND VARCOPE FILES TO BE MEREGED
+#
+###########
+# directory where preprocessed fMRI data is located
+baseDir = os.path.join(dataDir, 'BatchOutput_FingerFootLips/feat_dir')
+
+# a list of subjects
+subject_list = ['%02d' % i for i in range(1,11)]
+
+listCopeFiles = []
+listVarcopeFiles = []
+for iSubj in subject_list:
+    # full path to a cope image
+    pathCope = os.path.join(baseDir,
+                            'sub-' + iSubj,
+                            'ses-' + indSes,
+                            'run0.feat',
+                            'stats',
+                            'cope' + indCope + '.nii.gz')
+    listCopeFiles.append(pathCope)
+
+    # full path to a varcope image
+    pathVarcope = os.path.join(baseDir,
+                            'sub-' + iSubj,
+                            'ses-' + indSes,
+                            'run0.feat',
+                            'stats',
+                            'varcope' + indCope + '.nii.gz')
+    listVarcopeFiles.append(pathVarcope)
+
+
+
+###########
+#
+# NODES FOR THE WORKFLOW
+#
+###########
+copemerge = Node(fsl.Merge(dimension='t',
+                           in_files=listCopeFiles),
+                 name="copemerge")
+
+varcopemerge = Node(fsl.Merge(dimension='t',
+                           in_files=listVarcopeFiles),
+                    name="varcopemerge")
+
+# creating datasink to collect outputs
+datasink = Node(DataSink(base_directory=outDir),
+                name='datasink')
+
+
+###########
+#
+# SETTING UP THE WORKFLOW NODES
+#
+###########
+
+# creating the workflow
+mergeCopes = Workflow(name="Level1", base_dir=outDir)
+
+# connecting nodes
+mergeCopes.connect(copemerge, 'merged_file', datasink, 'copeMerged')
+mergeCopes.connect(varcopemerge, 'merged_file', datasink, 'varcopeMerged')
+
+
+# running the workflow
+mergeCopes.run()
