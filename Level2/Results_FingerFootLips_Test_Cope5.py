2b9534339869a9d433b3b95522c56b20463a2cfb
Satoru Hayasaka <hayasaka@utexas.edu>
2019-10-18 13:02:23 -0500

    cluster size table is generated; visuzliation has been added

--- Level2/Results_FingerFootLips_Test_Cope5.py
+++ Level2/Results_FingerFootLips_Test_Cope5.py
@@ -47,3 +47,47 @@ clusterWF.connect(cluster, 'index_file', datasink, 'index_file')
 clusterWF.connect(cluster, 'threshold_file', datasink, 'threshold_file')
 clusterWF.connect(cluster, 'localmax_txt_file', datasink, 'localmax_txt_file')
 clusterWF.run()
+
+
+
+# CALCULATING CLUSTER SIZES
+fClusInd = os.path.join(statsDir,'index_file/zstat1_index.nii.gz')
+X_ClusInd = nib.load(fClusInd).get_data()  # getting the image of cluster IDs
+clusterIDs = np.arange(1,X_ClusInd.max()+1)  # unique cluster IDs
+print('Cluster ID\tCluster Size')
+for iCluster in clusterIDs:
+    clusterSize = len(X_ClusInd[X_ClusInd==iCluster])
+    print('%3d' % iCluster + '\t\t%5d' % clusterSize)
+
+
+
+
+# VISUALIZATION
+# T1 weighted image for background
+anatDir = os.path.join(dataDir,'derivatives_selected/fmriprep/sub-09/anat/')
+imageT1 = os.path.join(anatDir,
+                       'sub-09_space-MNI152NLin2009cAsym_desc-preproc_T1w.nii.gz')
+
+# Thresholded zstat image
+fThZStat=os.path.join(statsDir,'threshold_file/zstat1_threshold.nii.gz')
+thImageStat=nib.load(fThZStat)
+
+# global maximum cooridnates
+X_zstat = nib.load(imgZStat).get_data()  # loading the zstat image
+globalMax = np.unravel_index(np.argmax(X_zstat), X_zstat.shape) # voxel space
+globalMaxMNI = coord_transform(globalMax[0],
+                               globalMax[1],
+                               globalMax[2],
+                               thImageStat.affine))  # MNI space
+
+# blob overlay at global max
+plot_stat_map(thImageStat, bg_img=imageT1,
+              colorbar=True, threshold=2.3, black_bg=True,
+              draw_cross=True,
+              cut_coords=globalMaxMNI)
+
+# interactive visualization
+view_img(thImageStat, bg_img=imageT1, cmap='black_red',
+         symmetric_cmap=False, annotate=True,
+         colorbar=True, threshold=2.3, black_bg=True,
+         cut_coords=globalMaxMNI)
