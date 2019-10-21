9fb8ff870221007a698dd67efc3e798ddb818ab7
Satoru Hayasaka <hayasaka@utexas.edu>
2019-10-21 12:16:55 -0500

    Results for FingerFootLips task, test session, contrast 6 (foot vs others), handedness comparison

--- Level2/Results_FingerFootLips_Test_Cope6_Handedness.py
+++ Level2/Results_FingerFootLips_Test_Cope6_Handedness.py
@@ -12,6 +12,7 @@ from nilearn.image import math_img, coord_transform
 
 # PARAMETERS
 zThresh = 2.3  # a.k.a., p=0.01 uncorrected threshold
+contInd = '1'  # contrast of interest
 
 # FILE AND DIRECTORY BUSINESS
 # original data directory
@@ -19,11 +20,11 @@ dataDir = '/tmp/Data/ds114'
 # Output directory
 outDir = os.path.join(dataDir,'WorkflowOutput')
 # Datasink directory
-datasinkDir = os.path.join(outDir,'FingerFootLips_Test_Cope5')
+datasinkDir = os.path.join(outDir,'FingerFootLips_Test_Cope6_Handedness')
 # stats_dir directory
 statsDir = os.path.join(datasinkDir,'stats_dir/stats')
 # Z-stat image
-imgZStat = os.path.join(statsDir, 'zstat1.nii.gz')
+imgZStat = os.path.join(statsDir, 'zstat' + condInd + '.nii.gz')
 
 
 # FINDING CLUSTERS IN THE ANALYSIS RESULTS
@@ -48,7 +49,7 @@ clusterWF.run()
 
 
 # LOADING CLUSTER MAXIMA TABLE
-fMaxTable = os.path.join(statsDir,'localmax_txt_file/zstat1_localmax.txt')
+fMaxTable = os.path.join(statsDir,'localmax_txt_file/zstat' + condInd + '_localmax.txt')
 maxData = pd.read_csv(fMaxTable, sep='\t')   # reading the maxima file as a dataframe
 maxData.dropna(how='all', axis=1, inplace=True)  # removing empty columns
 print(maxData)
@@ -56,7 +57,7 @@ print(maxData)
 
 
 # CALCULATING CLUSTER SIZES
-fClusInd = os.path.join(statsDir,'index_file/zstat1_index.nii.gz')
+fClusInd = os.path.join(statsDir,'index_file/zstat' + condInd + '_index.nii.gz')
 X_ClusInd = nib.load(fClusInd).get_data()  # getting the image of cluster IDs
 clusterIDs = np.arange(1,X_ClusInd.max()+1)  # unique cluster IDs
 print('Cluster ID\tCluster Size')
@@ -74,7 +75,7 @@ imageT1 = os.path.join(anatDir,
                        'sub-09_space-MNI152NLin2009cAsym_desc-preproc_T1w.nii.gz')
 
 # Thresholded zstat image
-fThZStat=os.path.join(statsDir,'threshold_file/zstat1_threshold.nii.gz')
+fThZStat=os.path.join(statsDir,'threshold_file/zstat' + condInd + '_threshold.nii.gz')
 thImageStat=nib.load(fThZStat)
 
 # global maximum cooridnates
