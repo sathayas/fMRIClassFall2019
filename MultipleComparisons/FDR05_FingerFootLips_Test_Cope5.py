610ab66e60b7ded5c14203fdfa60f4c5968a2212
Satoru Hayasaka <hayasaka@utexas.edu>
2019-10-29 09:10:41 -0500

    creates the FDR directory now, it it doesnt exist already

--- MultipleComparisons/FDR05_FingerFootLips_Test_Cope5.py
+++ MultipleComparisons/FDR05_FingerFootLips_Test_Cope5.py
@@ -1,5 +1,7 @@
 import os
 
+##### PARAMETERS #####
+
 ##### DIRECTORY BUSINESS ######
 # original data directory
 dataDir = '/tmp/Data/ds114'
@@ -8,3 +10,11 @@ wfDir = os.path.join(dataDir,'WorkflowOutput')
 statDir = os.path.join(wfDir,'FingerFootLips_Test_Cope5/stats_dir/stats/')
 # FDR results directory
 fdrDir = os.path.join(statDir,'FDR')
+
+# create the FDR directory if it doesnt already exist
+if not os.path.exists(fdrDir):
+        os.makedirs(fdrDir)
+
+
+##### T-STATISTIC IMAGE TO P-VALUE IMAGE #####
+# First, re-creating the design
