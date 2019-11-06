9ce383aaa65117b00101a773803edeefe4875898
Satoru Hayasaka <hayasaka@utexas.edu>
2019-11-06 15:40:39 -0600

    Added scatter plot of data

--- MachineLearning/IrisCluster.py
+++ MachineLearning/IrisCluster.py
@@ -8,10 +8,30 @@ from sklearn import datasets
 iris = datasets.load_iris()
 X = iris.data    # Data
 y = iris.target  # Target i.e., true clusters
-numClus = 3  # number of clusters
+nVar = X.shape[1]  # number of features
+
+# plotting the data
+plt.figure(figsize=[6, 6])
+
+# generating subplots
+for iRow in range(nVar-1):  # subplot row index
+    for iCol in range(iRow+1, nVar):  # subplot column index
+        indPlot = (nVar-1)*iRow + iCol
+        plt.subplot(nVar-1, nVar-1, indPlot)
+        plt.plot(X[:,iCol], X[:,iRow], 'b.')
+        if iCol==iRow+1:
+            plt.xlabel(varNames[iCol])
+            plt.ylabel(varNames[iRow])
+
+# adjusting the space between subplots
+plt.subplots_adjust(wspace=0.35, hspace=0.35)
+plt.show()
+
+
 
 # K-means clustering
-km = KMeans(n_clusters=3)  # defining the clustering object
+numClus = 3  # number of clusters
+km = KMeans(n_clusters=numClus)  # defining the clustering object
 km.fit(X)  # actually fitting the data
 y_clus = km.labels_   # clustering info resulting from K-means
 y_cent = km.cluster_centers_  # centroid coordinates
@@ -34,4 +54,3 @@ plt.ylabel('Sepal length')
 plt.title('True clusters')
 
 plt.show()
-
