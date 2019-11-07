3a95200c2995706f7d57267e215eda252a414961
Satoru Hayasaka <hayasaka@utexas.edu>
2019-11-07 11:27:50 -0600

    Removed PCA; plot is Feature 1 vs 9

--- MachineLearning/WiscCluster.py
+++ MachineLearning/WiscCluster.py
@@ -1,34 +1,21 @@
+import os
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from sklearn.decomposition import PCA
 from sklearn.cluster import KMeans
 
 # loadin the data
-wiscData = pd.read_csv('wiscsem.txt', sep='\t')
+wiscData = pd.read_csv(os.path.join('DataML','wiscsem.txt'), sep='\t')
 wiscFeatures = np.array(wiscData.iloc[:,2:13])
 featureNames = np.array(wiscData.columns[2:13])
-
-# applying PCA
-pca = PCA()  # creating a PCA transformation object
-wiscPCs = pca.fit_transform(wiscFeatures) # fit the data
-
-# Squared error plot
-plt.plot(np.arange(1,len(featureNames)+1), pca.explained_variance_ratio_)
-plt.xlabel('Component number')
-plt.ylabel('Explained variance')
-plt.show()
-
-## We will use first two components
-PC = wiscPCs[:,:2]
 
 
 # determinging the number of clusters (up to 20 clusters)
 SSE = []
-for iClus in range(1,21):  
+for iClus in range(1,21):
     # K-means clustering
     km = KMeans(n_clusters=iClus)  # K-means with a given number of clusters
-    km.fit(PC)  # fitting the principal components
+    km.fit(wiscFeatures)  # fitting the principal components
     SSE.append(km.inertia_) # recording the sum of square distances
 
 # plotting the sum of square distance
@@ -40,25 +27,18 @@ plt.show()
 
 ## We will go with 3 clusters
 # K-means clustering again
-km = KMeans(n_clusters=3)  
-km.fit(PC)  # fitting the principal components
+km = KMeans(n_clusters=3)
+km.fit(wiscFeatures)  # fitting the principal components
 y_clus = km.labels_   # clustering info resulting from K-means
 
 
 ### plotting the clusters
-plt.figure(figsize=[8,4])
-# First, in PC space
-plt.subplot(121)
-plt.scatter(PC[:,0],PC[:,1],c=y_clus,marker='+')
-plt.xlabel('PC1')
-plt.ylabel('PC2')
-plt.title('Clusters from K-means (PC)')
-
 # with two of the features
-plt.subplot(122)
-plt.scatter(wiscFeatures[:,1],wiscFeatures[:,4],c=y_clus,marker='+')
-plt.xlabel('comp')
-plt.ylabel('vocab')
-plt.title('Clusters from K-means (features)')
+xFeature = 1 # index for the feature on the x-axis
+yFeature = 9 # index for the feature on the y-axis
+plt.scatter(wiscFeatures[:,xFeature],
+            wiscFeatures[:,yFeature],c=y_clus,marker='+')
+plt.xlabel(featureNames[xFeature])
+plt.ylabel(featureNames[yFeature])
+plt.title(featureNames[xFeature] + ' v.s. ' + featureNames[yFeature])
 plt.show()
-
