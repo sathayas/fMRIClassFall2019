df3b6428c4b73a600073f4ac78ed832d6c553697
Satoru Hayasaka <hayasaka@utexas.edu>
2019-11-08 10:07:16 -0600

    no longer include boundary functions

--- MachineLearning/IrisKNN.py
+++ MachineLearning/IrisKNN.py
@@ -4,45 +4,7 @@ from sklearn import datasets
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix, classification_report
-
-
-def make_meshgrid(x, y, h=.02):
-    """Create a mesh of points to plot in
-
-    Parameters
-    ----------
-    x: data to base x-axis meshgrid on
-    y: data to base y-axis meshgrid on
-    h: stepsize for meshgrid, optional
-
-    Returns
-    -------
-    xx, yy : ndarray
-    """
-    x_min, x_max = x.min() - 1, x.max() + 1
-    y_min, y_max = y.min() - 1, y.max() + 1
-    xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
-                         np.arange(y_min, y_max, h))
-    return xx, yy
-
-
-def plot_contours(ax, clf, x, y, h=.02, **params):
-    """Plot the decision boundaries for a classifier.
-
-    Parameters
-    ----------
-    ax: matplotlib axes object
-    clf: a classifier
-    x: data to base x-axis meshgrid on
-    y: data to base y-axis meshgrid on
-    h: stepsize for meshgrid, optional
-    params: dictionary of params to pass to contourf, optional
-    """
-    xx, yy = make_meshgrid(x, y, h)
-    Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
-    Z = Z.reshape(xx.shape)
-    out = ax.contourf(xx, yy, Z, **params)
-    return out
+from DecisionBoundary import plot_contours   # plotting decison boundary
 
 
 # Loading the iris data
@@ -123,7 +85,7 @@ plt.title('Decision boundaries\n(k=20, distance)')
 plt.xlabel(feature_names[0])
 plt.ylabel(feature_names[3])
 
-plt.subplots_adjust(hspace=0.3, wspace=0.275, 
+plt.subplots_adjust(hspace=0.3, wspace=0.275,
                     bottom=0.05, top=0.95, left=0.10, right=0.975)
 plt.show()
 
