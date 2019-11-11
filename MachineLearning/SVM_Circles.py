6da65652b9ae2660ab8f8b672dbcecea9c47ceca
Satoru Hayasaka <hayasaka@utexas.edu>
2019-11-11 08:57:25 -0600

    Now import margin plot function from an external file

--- MachineLearning/SVM_Circles.py
+++ MachineLearning/SVM_Circles.py
@@ -2,40 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.datasets.samples_generator import make_circles
 from sklearn.svm import SVC
+from DecisionBoundary import plot_svm_margin
 
-def plot_svm_margin(X,y,sv):
-    '''
-    Input:
-         X:  2D data matrix
-         y:  Target vector
-         sv: Support vector machine results
-    Returns:
-         None
-    Produces:
-         A scatter plot of the data X, with colors defined by y.
-         SVM boundary, as well as the margin is plotted. Also
-         support vectors are indicated.
-
-         You need to run plt.show() after everything is done.
-    '''
-    # scatter plot first
-    plt.scatter(X[:, 0], X[:, 1], c=y, s=50, cmap=plt.cm.coolwarm)
-    # suppor for the meshgrid
-    ax = plt.gca()
-    xlim = ax.get_xlim()
-    ylim = ax.get_ylim()
-    # create grid to evaluate model
-    xx = np.linspace(xlim[0], xlim[1], 30)
-    yy = np.linspace(ylim[0], ylim[1], 30)
-    YY, XX = np.meshgrid(yy, xx)
-    xy = np.vstack([XX.ravel(), YY.ravel()]).T
-    Z = sv.decision_function(xy).reshape(XX.shape)
-    # plot decision boundary and margins
-    ax.contour(XX, YY, Z, colors='k', levels=[-1, 0, 1], alpha=0.5,
-               linestyles=['--', '-', '--'])
-    # plot support vectors
-    ax.scatter(sv.support_vectors_[:, 0], sv.support_vectors_[:, 1], s=200,
-               linewidth=1, edgecolors='k', facecolors='none')
 
 
 # Creating a toy data with circles
